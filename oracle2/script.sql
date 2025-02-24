c3f4b7bbea33536e6d65db5f9a62f61f5104a1aa
olrij <joseantonio.olmedo@iespoligonosur.org>
2025-02-24 23:26:59 +0100

    Update script.sql
    Apartados c, d, e y f

--- oracle2/script.sql
+++ oracle2/script.sql
@@ -31,11 +31,21 @@ CREATE TABLE GRUPOS2(
 )NESTED TABLE PERSONA_ANIDADA STORE AS PERSONA_ANIDADA_TABLE;
 /
 INSERT INTO GRUPOS2 VALUES(1,'INFORMÁTICA','21/01/2020',GRUPOS_T(PERSONA(1, 'Juan', Direccion('c/ Conde','Málaga',12001),'17/02/1990'),PERSONA(2, 'María', Direccion('c/ Amiel','Córdoba',16001),'21/10/1991'),PERSONA(3, 'Sofía', Direccion('c/ Castillo','Cádiz',15001),'1/09/1992'),PERSONA(4, 'Carla', Direccion('c/ Sin nombre','Cádiz',11001),'14/08/1990'),PERSONA(5, 'Manuel', Direccion('c/ Cuba','Málaga',10001),'27/02/1989')));
-
 INSERT INTO GRUPOS2 VALUES(2,'ADMINISTRACIÓN','2/02/2020',GRUPOS_T(PERSONA(6, 'Lucas', DirecciOn('c/ España','Granada',11001),'29/01/1988'),PERSONA(7, 'Marta', DirecciOn('c/ Conde','Málaga',12001),'30/07/1986'),PERSONA(8, 'Carmen', DirecciOn('c/ Mimbre','Granada',11001),'1/04/1990'),PERSONA(9, 'Milagros', DirecciOn('c/ Segura','Sevilla',16001),'7/01/1994')));
 INSERT INTO GRUPOS2 VALUES(3,'DIRECCIÓN','10/03/2020',GRUPOS_T(PERSONA(10, 'José Miguel', DirecciOn('c/ Volga','Sevilla',15001),'5/12/1979'),PERSONA(11, 'Antonia', DirecciOn('c/ Tajo','Málaga',15001),'17/05/1980')));
-delete from grupos2;
-COMMIT;
+
+-- Actualiza la tabla anidada de personas 
+-- asociadas al grupo “DIRECCIÓN” para añadir una nueva persona.
+INSERT INTO TABLE (SELECT PERSONA_ANIDADA FROM GRUPOS2 GP WHERE GP.NOMBRE='DIRECCIÓN') VALUES (PERSONA(100, 'Manuel', Direccion('c/ Cuba','Málaga',10001),'27/02/1989'));
+-- Actualiza la tabla anidada de personas asociadas al grupo “DIRECCIÓN” 
+-- para eliminar a la persona con código 8.
+-- EN vez de borrar el 
+DELETE FROM TABLE (SELECT PERSONA_ANIDADA FROM GRUPOS2 GP WHERE GP.NOMBRE='ADMINISTRACIÓN') ADM WHERE ADM.CODIGO=8;
+-- Modifica la fecha de creación del grupo con id 2 para que sea ‘20/05/2021’.
+UPDATE GRUPOS2 SET FECHA_CREACION='20/05/2021' WHERE ID=2;
+-- Borra el grupo INFORMÁTICA.
+DELETE FROM GRUPOS2 WHERE NOMBRE='INFORMÁTICA';
+
+-- Comprobación
 select * from grupos2;
 
-SELECT * FROM GRUPOS2 GR, TABLE(PERSONA_ANIDADA) P WHERE P.DIREC.CIUDAD='Málaga';
